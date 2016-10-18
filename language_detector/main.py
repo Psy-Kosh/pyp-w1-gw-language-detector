e53e73b068fdfaad956d0c599a3dd8d1936f87ff
contrepoint <contrepoint21@gmail.com>
2016-10-18 23:38:37 +0000

    works despite cloud9 difficulties

--- language_detector/main.py
+++ language_detector/main.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
-
-"""This is the entry point of the program."""
-
 def detect_language(text, languages):
     """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    counts = { lang['name']:0 for lang in languages }
+    words = text.split()
+    for w in words:
+        for lang in languages:
+            if w in lang['common_words']:
+                counts[lang['name']]+=1
+    return max((count, name) for name, count in counts.items())[1]
